23981f6c918672b9770df65e84fc787428e5e5d8
Konrad Dębski <k.debski@empressia.pl>
2025-12-06 21:55:50 +0100

    feat: pass Day 2, part 1 challenge

--- src/Day-2/day2-script.ts
+++ src/Day-2/day2-script.ts
@@ -3,12 +3,32 @@ import { readInput } from "../utils/index.js"
 const DAY = 2
 const input = readInput(DAY, "input.txt")
 
-console.log("📝 Input:\n")
-console.log(input)
-console.log("========================")
+function result(input: string): number {
+  const rangesArr: string[] = input.split(',')
 
-function result(_input: string): number {
-  return 0
+  const invalidIDsArr: number[] = []
+
+  rangesArr.forEach((range) => {
+    const [minValueStr, maxValueStr] = range.split('-')
+    const minValueInt = parseInt(minValueStr)
+    const maxValueInt = parseInt(maxValueStr)
+
+    for (let i = minValueInt; i <= maxValueInt; i++) {
+      const currentValueStr = i.toString()
+      const currentValueStrLength = currentValueStr.length
+      if (currentValueStrLength % 2 !== 0) continue
+
+      const currentValueStrMiddle = Math.floor(currentValueStrLength / 2)
+      const currentValueFirstHalf = currentValueStr.slice(0, currentValueStrMiddle)
+      const currentValueSecondHalf = currentValueStr.slice(currentValueStrMiddle, currentValueStrLength)
+
+      if (currentValueFirstHalf !== currentValueSecondHalf) continue
+
+      invalidIDsArr.push(i)
+    }
+  })
+
+  return invalidIDsArr ? invalidIDsArr.reduce((sum, currentValue) => sum += currentValue) : 0
 }
 
 // ====================================
@@ -17,4 +37,4 @@ function result(_input: string): number {
 console.log(`🎄 Advent of Code - Day ${DAY}`)
 
 console.log("========================\n")
-console.log("Times pointed at 0: %d", result(input))
+console.log("Result after adding all invalid IDs: %d", result(input))
